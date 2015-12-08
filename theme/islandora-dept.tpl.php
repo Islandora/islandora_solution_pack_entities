ef3b3431c2dfd1807ca00b6a3f6fff8f32875537
Rosie Le Faive <lefaive@gmail.com>
2015-12-08 18:59:26 -0400

    ISLANDORA-1485 Display TN in department object view.

--- theme/islandora-dept.tpl.php
+++ theme/islandora-dept.tpl.php
@@ -1,11 +1,19 @@
 <?php
 /**
  * @file
- * This is the template file for the object page for person objects.
+ * This is the template file for the object page for organization objects.
  */
 ?>
+
+<div class="islandora-object-image islandora-department-thumbnail">
+  <?php if (isset($variables['tn'])): ?>
+    <img src="<?php print $variables['tn']; ?>"/>
+  <?php endif; ?>
+</div>
+
 <?php if (isset($variables['metadata'])): ?>
   <div class="departmental_metadata">
     <?php print $variables['metadata']; ?>
   </div>
 <?php endif; ?>
+
